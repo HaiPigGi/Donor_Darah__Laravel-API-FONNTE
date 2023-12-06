f49ffc8352042b74373c02bdeac411a42fcb46f1
HaiPigGi <leonardobryan32@gmail.com>
2023-12-06 21:11:54 +0700

    update code for maps

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -36,49 +36,101 @@ class UserController extends Controller
         }
     }
 
-    public function getUserLocation($userId)
+    // public function getUserLocation($userId)
+    // {
+    //     try {
+    //         // Retrieve the user's profile
+    //         $profile = profileModel::where('id_user', $userId)->first();
+
+    //         if (!$profile) {
+    //             return response()->json(['error' => 'User profile not found'], 404);
+    //         }
+
+    //         // Retrieve kelurahan data
+    //         $kelurahanData = Kelurahan::find($profile->kelurahan_id);
+
+    //         if (!$kelurahanData) {
+    //             return response()->json(['error' => 'Kelurahan not found'], 404);
+    //         }
+
+    //         // Retrieve kecamatan data
+    //         $kecamatanData = Kecamatan::find($kelurahanData->kecamatan_id);
+
+    //         if (!$kecamatanData) {
+    //             return response()->json(['error' => 'Kecamatan not found'], 404);
+    //         }
+
+    //         // Retrieve kabupaten data with latitude and longitude
+    //         $kabupatenData = Kabupaten::find($kecamatanData->kabupaten_id);
+
+    //         if (!$kabupatenData) {
+    //             return response()->json(['error' => 'Kabupaten not found'], 404);
+    //         }   
+    //         // Return the data
+    //         return response()->json([
+    //             'kabupaten' => [
+    //                 'id' => $kabupatenData->id,
+    //                 'name' => $kabupatenData->nama,
+    //                 'lat' => $kabupatenData->lat,
+    //                 'long' => $kabupatenData->long,
+    //             ],
+    //         ]);
+    //     } catch (\Exception $e) {
+    //         return response()->json(['error' => 'An error occurred. Please try again.'], 500);
+    //     }
+    // }
+    public function getUserLocation()
     {
         try {
-            // Retrieve the user's profile
-            $profile = profileModel::where('id_user', $userId)->first();
-
-            if (!$profile) {
-                return response()->json(['error' => 'User profile not found'], 404);
-            }
-
-            // Retrieve kelurahan data
-            $kelurahanData = Kelurahan::find($profile->kelurahan_id);
-
-            if (!$kelurahanData) {
-                return response()->json(['error' => 'Kelurahan not found'], 404);
-            }
-
-            // Retrieve kecamatan data
-            $kecamatanData = Kecamatan::find($kelurahanData->kecamatan_id);
-
-            if (!$kecamatanData) {
-                return response()->json(['error' => 'Kecamatan not found'], 404);
-            }
-
-            // Retrieve kabupaten data with latitude and longitude
-            $kabupatenData = Kabupaten::find($kecamatanData->kabupaten_id);
-
-            if (!$kabupatenData) {
-                return response()->json(['error' => 'Kabupaten not found'], 404);
-            }   
-            // Return the data
-            return response()->json([
-                'kabupaten' => [
-                    'id' => $kabupatenData->id,
-                    'name' => $kabupatenData->nama,
+            // Retrieve all profiles
+            $profiles = profileModel::all();
+    
+            // Initialize an empty array to store user locations
+            $userLocations = [];
+    
+            foreach ($profiles as $profile) {
+                // Retrieve kelurahan data
+                $kelurahanData = Kelurahan::find($profile->kelurahan_id);
+    
+                if (!$kelurahanData) {
+                    // Skip to the next iteration if kelurahan not found
+                    continue;
+                }
+    
+                // Retrieve kecamatan data
+                $kecamatanData = Kecamatan::find($kelurahanData->kecamatan_id);
+    
+                if (!$kecamatanData) {
+                    // Skip to the next iteration if kecamatan not found
+                    continue;
+                }
+    
+                // Retrieve kabupaten data with latitude and longitude
+                $kabupatenData = Kabupaten::find($kecamatanData->kabupaten_id);
+    
+                if (!$kabupatenData) {
+                    // Skip to the next iteration if kabupaten not found
+                    continue;
+                }
+    
+                // Add the user location to the array
+               // Add the user location to the array
+                $userLocations[] = [
                     'lat' => $kabupatenData->lat,
                     'long' => $kabupatenData->long,
-                ],
-            ]);
+                ];
+
+                // Return the data
+
+                }
+
+                return response()->json(['user_locations' => $userLocations]);
+
         } catch (\Exception $e) {
             return response()->json(['error' => 'An error occurred. Please try again.'], 500);
         }
     }
+    
 
 
     
--- my-app/src/_components/Map/Map.js
+++ my-app/src/_components/Map/Map.js
@@ -1,21 +1,60 @@
 "use client"
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
-// import gambar from "@/assets/iconArrowDown.png";
+import "@/_styles/css/map.css";
+function Map() {
+  const [userLocations, setUserLocations] = useState([]);
 
-function Map(){
-    return(
-        <MapContainer className="w-full h-full rounded-3xl" center={[-0.502183, 117.153801]} zoom={13} scrollWheelZoom={false} height={"50rem"}>
-            <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[-0.502183, 117.153801]}>
-            <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-            </Marker>
-        </MapContainer>
-    )
+  useEffect(() => {
+    const fetchUserLocations = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/user/map");
+        setUserLocations(response.data.user_locations);
+      } catch (error) {
+        console.error("Error fetching user locations:", error);
+      }
+    };
+
+    fetchUserLocations();
+  }, []);
+
+
+  // Extracting latitudes and longitudes from userLocations
+  const coordinates = userLocations.map(({ lat, long }) => [parseFloat(lat), parseFloat(long)]);
+
+  // Calculate the bounds of all coordinates
+  const bounds = coordinates.length > 0 ? L.latLngBounds(coordinates) : null;
+
+  return (
+
+    <MapContainer className="w-full h-full rounded-3xl" center={[-0.8688889, 109.9072559]} zoom={4} scrollWheelZoom={false} height={"50rem"} bounds={bounds}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {/* Map through userLocations and create Marker for each user */}
+      {userLocations.map((userLocation) => (
+        <Marker  key={userLocation.user_id}
+        position={[parseFloat(userLocation.lat), parseFloat(userLocation.long)]}
+        className="custom-marker"  // Apply the custom CSS class
+      >
+          <Popup>
+            User ID: {userLocation.user_id}
+            <br />
+            Lat: {userLocation.lat}
+            <br />
+            Long: {userLocation.long}
+          </Popup>
+        </Marker>
+      ))}
+
+      {/* Draw polyline connecting all waypoints */}
+      <Polyline positions={coordinates} color="red" />
+    </MapContainer>
+  );
 }
+
 export default Map;

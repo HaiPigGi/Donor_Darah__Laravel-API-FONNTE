9f4203352e25235d2f39b5203da371d344c29fdc
HaiPigGi <leonardobryan32@gmail.com>
2023-12-09 20:44:25 +0700

    update code

--- my-app/src/_components/navbar.js
+++ my-app/src/_components/navbar.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import { Dropdown } from "react-bootstrap";
 import Link from "next/link";
 import axios from "axios";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function Navbar(props) {
   const [userId, setUserId] = useState(null);
@@ -132,88 +135,176 @@ const handleLogout = async () => {
   };
 
 
-  const handleDropdownToggle = (isOpen) => {
-    setShowDropdown(isOpen);
-  };
-  useEffect(() => {
+  const handleDropdownToggle = () => {
+    const dropDownBasic = document.getElementById("Menus");
+    setIsOpen(!isOpen);
+
+    if (isOpen) {
+        dropDownBasic.classList.add("from:transform");
+        dropDownBasic.classList.add("from:opacity-0");
+        dropDownBasic.classList.add("from:scale-95");
+        dropDownBasic.classList.add("to:transform");
+        dropDownBasic.classList.add("to:opacity-100");
+        dropDownBasic.classList.add("to:scale-100");
+    } else {
+        dropDownBasic.classList.remove("from:transform");
+        dropDownBasic.classList.remove("from:opacity-0");
+        dropDownBasic.classList.remove("from:scale-95");
+        dropDownBasic.classList.remove("to:transform");
+        dropDownBasic.classList.remove("to:opacity-100");
+        dropDownBasic.classList.remove("to:scale-100");
+
+        dropDownBasic.classList.add("from:");
+    }
+};
+useEffect(() => {
     const handleDocumentClick = (e) => {
-      // Cek apakah element yang diklik adalah bagian dari dropdown
-      if (e.target.closest("#dropdown-container")) {
-        return;
-      }
-      
-      // Jika tidak, tutup dropdown
-      setShowDropdown(false);
+        // Cek apakah element yang diklik adalah bagian dari dropdown
+        if (e.target.closest("#dropdown-container")) {
+            return;
+        }
+
+        // Jika tidak, tutup dropdown
+        setShowDropdown(false);
     };
-  
+
     // Tambahkan event listener pada dokumen
     document.addEventListener("click", handleDocumentClick);
-  
+
     // Membersihkan event listener saat komponen dibongkar
     return () => {
-      document.removeEventListener("click", handleDocumentClick);
+        document.removeEventListener("click", handleDocumentClick);
     };
-  }, []);
+}, []);
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
+
   
   return (
     <nav className="absolute top-0 w-full z-50">
-      <div className="md:container mx-auto flex flex-col md:flex-row py-5 items-center justify-start md:justify-between">
-        <div id="brand" className="mb-4 md:mb-0 border-2">
-          <a href="/" className="text-red text-3xl font-black font-Title">
-            Dondar
-          </a>
+    <div className="md:container mx-auto flex flex-col md:flex-row py-5 items-center justify-start md:justify-between">
+        <div id="brand" className="mb-4 md:mb-0">
+            <a
+                href="/"
+                className="text-red text-3xl font-black font-Title"
+            >
+                Dondar
+            </a>
         </div>
         <div
-          id="items"
-          className={["font-Subtitle font-regular md:visible collapse w-1/2 text-right", props.itemsColor].join(" ")}
+            id="items"
+            className={[
+                "font-Subtitle font-regular md:visible collapse w-1/2 text-right",
+                props.itemsColor,
+            ].join(" ")}
         >
-          <a
-            href="/FormPengajuan"
-            className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
-          >
-            Kebutuhan
-          </a>
-          <a
-            href="/forum"
-            className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
-          >
-            Forum
-          </a>
-          <a
-            href=""
-            className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
-          >
-            Tentang
-          </a>
-          {userId ? (
-            <Dropdown 
-            className="inline"
-             show={showDropdown}
-             onToggle={handleDropdownToggle} >
-              <Dropdown.Toggle
-                variant="success"
-                id="dropdown-basic"
-                className="px-4 py-2 bg-red text-white font-bold rounded-full ml-4"
-              >
-                {user ? `Hello, ${user.nama}` : 'Hello'} {/* Access the name directly from the user state */}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href={'/profile'}>
-                   View Profile
-                   </Dropdown.Item>
-                <br/>
-                <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <Link href="/Login" passHref legacyBehavior>
-              <a className="px-4 py-2 bg-red text-white font-bold rounded-full ml-4">
-                Login
-              </a>
-            </Link>
-          )}
+            <a
+                href="/FormPengajuan"
+                className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
+            >
+                Kebutuhan
+            </a>
+            <a
+                href="/forum"
+                className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
+            >
+                Forum
+            </a>
+            <a
+                href=""
+                className="px-4 py-2 hover:border-b-2 border-red hover:text-red"
+            >
+                Tentang
+            </a>
+            {userId ? (
+                // <Dropdown className="inline">
+                //     <Dropdown.Toggle
+                //         id="dropdown-basic"
+                //         className="px-4 py-2 bg-red text-white font-bold rounded-full ml-4"
+                //     >
+                //         {user ? `Hello, ${user.nama}` : "Hello"}{" "}
+                //         {/* Access the name directly from the user state */}
+                //     </Dropdown.Toggle>
+                //     <Dropdown.Menu>
+                //         <Dropdown.Item href={"/profile"}>
+                //             View Profile
+                //         </Dropdown.Item>
+                //         <br />
+                //         <Dropdown.Item onClick={handleLogout}>
+                //             Logout
+                //         </Dropdown.Item>
+                //     </Dropdown.Menu>
+                // </Dropdown>
+                <Menu
+                    as="div"
+                    className="relative inline-block text-left"
+                >
+                    <div>
+                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-red px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red">
+                            {user ? `Hello, ${user.nama}` : "Hello"}
+                            <ChevronDownIcon
+                                className="-mr-1 h-5 w-5 text-white"
+                                aria-hidden="true"
+                            />
+                        </Menu.Button>
+                    </div>
+
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="/profile"
+                                            className={classNames(
+                                                active
+                                                    ? "bg-gray-100 text-gray-900"
+                                                    : "text-gray-700",
+                                                "block px-4 py-2 text-sm",
+                                            )}
+                                        >
+                                            View Profile
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            onClick={handleLogout}
+                                            className={classNames(
+                                                active
+                                                    ? "bg-gray-100 text-gray-900"
+                                                    : "text-gray-700",
+                                                "block w-full px-4 py-2 text-left text-sm",
+                                            )}
+                                        >
+                                            Sign out
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            ) : (
+                <Link href="/Login" passHref legacyBehavior>
+                    <a className="px-4 py-2 bg-red text-white font-bold rounded-full ml-4">
+                        Login
+                    </a>
+                </Link>
+            )}
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
   );
 }
--- my-app/src/app/(pages)/FormPengajuan/page.jsx
+++ my-app/src/app/(pages)/FormPengajuan/page.jsx
@@ -3,167 +3,263 @@ import axios from "axios";
 import Navbar from "@/_components/navbar";
 import "@/_styles/css/login.css";
 import "@/_styles/css/regis.css";
-import { useState } from "react";
 import Hal1 from "./hal1";
 import Hal2 from "./hal2";
 import { useEffect } from "react";
 import Loading from "@/_components/Loading/Loading";
 import withAuth from "@/_components/Auth/WithAuth";
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+    ExclamationTriangleIcon,
+    CheckCircleIcon,
+} from "@heroicons/react/24/outline";
 
 const FormPengajuan = () => {
- 
-  const [data, setData] = useState({
-    nama: "",
-    telepon: "",
-    ktp: "",
-    golongan_darah: "",
-    jumlah_kantong: "",
-    kelurahan_id: "",
-});
-  const [session, setSession] = useState({});
-  const [buttonNext, setButtonNext] = useState(0);
+    const [session, setSession] = useState({});
+    const [buttonNext, setButtonNext] = useState(0);
 
-  const [loading, setLoading] = useState(true);
-  const [progress, setProgress] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    // Simulate a delay (e.g., API request)
-    const delay = setTimeout(() => {
-      setLoading(false);
-    }, 4500);
+    const [error, setError] = useState("");
 
-    // Update progress every 50ms until it reaches 100%
-    const progressInterval = setInterval(() => {
-      setProgress(prevProgress => (prevProgress < 100 ? prevProgress + 1 : prevProgress));
-    }, 50);
+    const [openError, setOpenError] = useState(false);
+    const cancelButtonRef = useRef(null);
 
-    // Cleanup the timeout and interval to avoid memory leaks
-    return () => {
-      clearTimeout(delay);
-      clearInterval(progressInterval);
+    // make use state for parsing the data
+    const [data, setData] = useState({
+        nama: "",
+        telepon: "",
+        ktp: "",
+        golongan_darah: "",
+        jumlah_kantong: "",
+        kelurahan_id: "",
+        tujuan_pengajuan: "",
+    });
+
+    const apiUrl = process.env.NEXT_PUBLIC_APP_URL_API;
+
+    useEffect(() => {
+        // Simulate a delay (e.g., API request)
+        const delay = setTimeout(() => {
+            setLoading(false);
+        }, 4500);
+
+        // Update progress every 50ms until it reaches 100%
+        const progressInterval = setInterval(() => {
+            setProgress((prevProgress) =>
+                prevProgress < 100 ? prevProgress + 1 : prevProgress,
+            );
+        }, 50);
+
+        // Cleanup the timeout and interval to avoid memory leaks
+        return () => {
+            clearTimeout(delay);
+            clearInterval(progressInterval);
+        };
+    }, []);
+
+    const getcsrf = async () => {
+        let cookie = await axios.get(`${apiUrl}/api/senctum/csrf-cookie`);
+        setSession(cookie);
     };
-  }, []);
-  
-  async function registrasi() {
-    try{
-      await axios.post(
-        `${apiUrl}/api/register/auth`,
-        data,{
-          headers: {
-            "csrf_token" : session.csrf_token,
-            "session_data" : session.session_data
-          }
+
+    // for checking is there still null values on object data
+    const cekAllFilled = () => {
+        console.log("openError from cekAllFilled : ", openError);
+        if (
+            data.nama == "" ||
+            data.ktp == "" ||
+            data.telepon == "" ||
+            data.golongan_darah == "" ||
+            data.kelurahan_id == "" ||
+            data.jumlah_kantong == ""
+        ) {
+            console.log(data);
+            setError("Ada data yang belum diisi");
+            setOpenError(true);
+            console.log("openError from cekAllFilled2 : ", openError);
+        } else {
+            setError("");
+            setOpenError(false);
+            sendForm();
         }
-        );
-      } catch (error) {
-        alert(error);
-      }
-    }
-    
-    useState({
-      registrasi
-    },[])
+    };
 
-  const getcsrf = async () => {
-    let cookie = await axios.get(`${apiUrl}/api/senctum/csrf-cookie`)
-    setSession(cookie)
-  }
+    // sending form to backend
+    const sendForm = async () => {
+        try {
+            await axios.post(`${apiUrl}/api/form/akseptor-send`, data, {
+                headers: {
+                    csrf_token: session.csrf_token,
+                    session_data: session.session_data,
+                },
+            });
+            setOpenError(true);
+        } catch (error) {
+            console.log("Error From formPengajuan Page.jsx : ", error);
+        }
+    };
 
-  const handleButton = () => {
-    setButtonNext(buttonNext + 1);
-    const pagi = document.getElementById("pagination").childNodes[buttonNext + 1];
-    console.log(pagi);
-    pagi.classList.remove("border-2");
-    pagi.classList.remove("border-red");
-    pagi.classList.add("bg-red");
-  }
-  
-  const handleKembali = () =>{
-    const pagi0 = document.getElementById("pagination").childNodes[buttonNext];
-    setButtonNext(buttonNext - 1);
-    pagi0.classList.remove("bg-red");
-    pagi0.classList.add("border-2");
-    pagi0.classList.add("border-red");
-  }
-
-  const Buttons = () => {
-    if(buttonNext == 0){
-     return  (
-      <div className="w-full flex justify-end py-2">
-            <button 
-                className="border-2 rounded-full px-3 py-3 bg-red text-white "
-                type="button"
-                onClick={handleButton}
+    const Buttons = () => {
+        return (
+            <div className="w-full flex justify-end py-2">
+                <button
+                    className="border-2 rounded-full px-3 py-3 bg-red text-white "
+                    type="button"
+                    onClick={cekAllFilled}
                 >
-               Selanjutnya
-            </button>
-      </div>
-     )
-
-    }else if(buttonNext == 1){
-      return (
-        <div className="w-full  flex justify-between py-2">
-            <button 
-                className="border-2 border-red rounded-full px-4 py-3 text-red "
-                type="button"
-                onClick={handleKembali}
-                >
-               kembali
-             </button>
-            <a 
-                className="border-2 rounded-full px-5 py-3 bg-red text-white "
-                href="/Dashboard"
-                >
-               submit
-             </a>
-        </div>
-      )
-    }
-  };
-
-  const Pages= () => {
-    if(buttonNext == 0){
-      return <Hal1/>
-    }else if(buttonNext == 1){
-      return <Hal2 />
-    }
-  }
-
-
-  return (
-    <section>
-      <div className="my-bg">
-      <div>
-          {loading ? (
-            <Loading progress={progress} />
-          ) : (
-            <div>
-        <Navbar itemsColor="text-white" />
-        <div className="row">
-            <div className="w-[70%] h-[80%] bg-white rounded-xl">
-              <div className="flex items-end justify-center w-full h-full text-center relative pb-[4%]">
-                <div className="absolute top-[4%] w-full">
-                  <h1 className="text-black font-Title text-[40px] block top-[4%] ">
-                    Form ajuan kebutuhan darah
-                  </h1>
-                  <div className=" flex justify-center items-center  mx-auto" id="pagination">
-                    <div className="w-14 h-3 bg-red me-2 rounded-full"></div>
-                    <div className="w-14 h-3  rounded-full border-2 border-red"></div>
-                  </div>
-                </div>
-                <form className="font-Subtitle  px-10">
-                      <Pages/>
-                      <Buttons />
-                </form>
-              </div>
+                    Kirim Pengajuan
+                </button>
             </div>
-          </div>
-          </div>
-          )}
-        </div>
-        </div>
-    </section>
-  );
-}
+        );
+    };
+
+    return (
+        <section>
+            <div className="my-bg">
+                <div>
+                    {loading ? (
+                        <Loading progress={progress} />
+                    ) : (
+                        <div>
+                            <Navbar itemsColor="text-white" />
+                            <div className=" h-screen flex items-center">
+                                <div className="w-[70%] h-auto bg-white rounded-xl mx-auto ">
+                                    <div className="mt-2 w-full">
+                                        <h1 className="text-black font-Title text-[40px] block text-center">
+                                            Form ajuan kebutuhan darah
+                                        </h1>
+                                    </div>
+                                    <div className="flex pt-2 justify-center w-full h-full text-center ">
+                                        <form className="font-Subtitle w-full px-5">
+                                            <Hal1
+                                            // for parsing data to children node
+                                                action={(newValue) => {
+                                                    setData(newValue);
+                                                }}
+                                                data={data}
+                                            />
+                                            <Buttons />
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            <Transition.Root show={openError} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="relative z-10"
+                    initialFocus={cancelButtonRef}
+                    onClose={setOpenError}
+                >
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            >
+                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                        <div className="sm:flex sm:items-start">
+                                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                {/* conditional rendering icon when error and success */}
+                                                {error ? (
+                                                    <ExclamationTriangleIcon
+                                                        className="h-6 w-6 text-red"
+                                                        aria-hidden="true"
+                                                    />
+                                                ) : (
+                                                    <CheckCircleIcon
+                                                        className="h-6 w-6 text-green-600"
+                                                        aria-hidden="true"
+                                                    />
+                                                )}
+                                            </div>
+                                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                <Dialog.Title
+                                                    as="h3"
+                                                    className="text-xl font-semibold leading-6 text-gray-900"
+                                                >
+                                                  {/* conditional rendering title when error and when success */}
+                                                    {error
+                                                        ? error
+                                                        : "Form berhasil dikirim"}
+                                                </Dialog.Title>
+                                                <div className="mt-2">
+                                                    <p className="text-sm text-gray-500">
+                                                      {/* conditional rendering subtitle when error and when success */}
+                                                        {error
+                                                            ? error
+                                                            : "kebutuhan darah akan dibroadcast ke seluruh pendonor"}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                        {error ? (
+                                            <button
+                                                type="button"
+                                                className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-white bg-red shadow-sm hover:bg-white hover:text-red hover:border-red hover:border-2 sm:ml-3 sm:w-auto"
+                                                onClick={() =>
+                                                    setOpenError(false)
+                                                }
+                                            >
+                                                Deactivate
+                                            </button>
+                                        ) : (
+                                            <a
+                                                type="button"
+                                                className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-white bg-green-600 shadow-sm hover:bg-white hover:text-green-600 hover:border-green-600 hover:border-2 sm:ml-3 sm:w-auto"
+                                                onClick={() =>
+                                                    setOpenError(false)
+                                                }
+                                                href="/"
+                                            >
+                                                Okee
+                                            </a>
+                                        )}
+
+                                        <button
+                                            type="button"
+                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                            onClick={() => setOpenError(false)}
+                                            ref={cancelButtonRef}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+        </section>
+    );
+};
 
 export default FormPengajuan;

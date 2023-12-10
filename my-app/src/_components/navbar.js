3ee3f6020fd6877aa693e1250f522b1e74864ebc
HaiPigGi <leonardobryan32@gmail.com>
2023-12-10 12:33:52 +0700

    Login Navbar

--- my-app/src/_components/navbar.js
+++ my-app/src/_components/navbar.js
@@ -275,6 +275,73 @@ function classNames(...classes) {
                         >
                             Tentang
                         </a>
+                        {userId ? (
+                <Menu
+                    as="div"
+                    className="relative inline-block text-left"
+                >
+                    <div>
+                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-red px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red">
+                            {user ? `Hello, ${user.nama}` : "Hello"}
+                            <ChevronDownIcon
+                                className="-mr-1 h-5 w-5 text-white"
+                                aria-hidden="true"
+                            />
+                        </Menu.Button>
+                    </div>
+
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="/profile"
+                                            className={classNames(
+                                                active
+                                                    ? "bg-gray-100 text-gray-900"
+                                                    : "text-gray-700",
+                                                "block px-4 py-2 text-sm",
+                                            )}
+                                        >
+                                            View Profile
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            onClick={handleLogout}
+                                            className={classNames(
+                                                active
+                                                    ? "bg-gray-100 text-gray-900"
+                                                    : "text-gray-700",
+                                                "block w-full px-4 py-2 text-left text-sm",
+                                            )}
+                                        >
+                                            Sign out
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            ) : (
+                <Link href="/Login" passHref legacyBehavior>
+                    <a className="px-4 py-2 bg-red text-white font-bold rounded-full ml-4">
+                        Login
+                    </a>
+                </Link>
+            )}
                         {/* Close button */}
                         <button
                             className="text-white px-4 py-2 mt-4 bg-red hover:bg-opacity-75 rounded-full"
